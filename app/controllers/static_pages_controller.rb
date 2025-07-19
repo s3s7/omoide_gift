ac57b74875ec929c5494ae42b2f6423bbff2ccb5
shun <samesamejinbei@gmail.com>
2025-07-20 08:37:26 +0900

    feat:画面修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,21 @@
 class StaticPagesController < ApplicationController
+  # Top page - landing page for the application
   def top
+  end
+
+  # How to use guide - explains how to use the gift recording service
+  def how_to_use
+  end
+
+  # Terms of service - legal terms and conditions
+  def terms
+  end
+
+  # Privacy policy - data protection and privacy information
+  def privacy
+  end
+
+  # Contact page - contact information and future contact form
+  def contact
   end
 end
