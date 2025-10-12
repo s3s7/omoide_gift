df041f454d41d84a95325509ebf1005dd3663afd
shun <samesamejinbei@gmail.com>
2025-10-12 20:49:57 +0900

    Lint:Lintエラー解消

--- db/migrate/20251012074807_add_position_to_gift_item_categories.rb
+++ db/migrate/20251012074807_add_position_to_gift_item_categories.rb
@@ -2,7 +2,7 @@ class AddPositionToGiftItemCategories < ActiveRecord::Migration[7.2]
   def change
     add_column :gift_item_categories, :position, :integer
 
-     # 既存データにpositionを設定
+    # 既存データにpositionを設定
     reversible do |dir|
       dir.up do
         GiftItemCategory.reset_column_information
