07479b5ad56e088dd2b53e11d563431ebd3e757d
shun <samesamejinbei@gmail.com>
2025-10-28 22:22:45 +0900

    refactor:マジックナンバーの定数化

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,11 +2,13 @@ class Relationship < ApplicationRecord
   include PositionMovable
   include DisplayNameable
 
+  NAME_MAX_LENGTH = 50
+
   # リレーション
   has_many :gift_people, dependent: :restrict_with_error
 
   # バリデーション
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: NAME_MAX_LENGTH }
   validates :name, uniqueness: { case_sensitive: false }
 
   # スコープ
