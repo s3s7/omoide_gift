0ad0fc7ac5a5c0be4f1ea86be3d5fd4644da5c19
shun <samesamejinbei@gmail.com>
2025-11-10 19:44:41 +0900

    fix:session_storeのコンマ抜け修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,5 +4,5 @@
 Rails.application.config.session_store :cookie_store,
   key: "_omoide_gift_session",
   secure: Rails.env.production?,
-  same_site: (Rails.env.production? ? :none : :lax)
+  same_site: (Rails.env.production? ? :none : :lax),
   domain: (Rails.env.production? ? ".meguri-gift.com" : nil)
