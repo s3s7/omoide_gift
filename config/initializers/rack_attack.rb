ac891a8cb9e26bb27252b97766375993631fb7f1
shun <samesamejinbei@gmail.com>
2025-11-10 19:41:45 +0900

    update:rack_attack.rb修正

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -8,8 +8,8 @@ class Rack::Attack
 
   # LINE ログインのコールバックは常に許可（部分的セーフリスト）
   # OmniAuth 2系はPOSTコールバックの可能性があるため、GET/POST両方を許可
-  CALLBACK_PATHS = %w[/users/auth/line/callback /auth/line/callback].freeze
-  safelist("allow LINE callback") do |req|
-    CALLBACK_PATHS.include?(req.path) && %w[GET POST].include?(req.request_method)
-  end
+  # CALLBACK_PATHS = %w[/users/auth/line/callback /auth/line/callback].freeze
+  # safelist("allow LINE callback") do |req|
+  #   CALLBACK_PATHS.include?(req.path) && %w[GET POST].include?(req.request_method)
+  # end
 end
