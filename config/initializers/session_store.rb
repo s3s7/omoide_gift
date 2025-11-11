53fcf36558c59ea43249d8ff98b5ae21ab63fed5
shun <samesamejinbei@gmail.com>
2025-11-11 19:38:36 +0900

    fix:Lineログイン修正

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Use secure, lax SameSite cookies in production to keep OmniAuth state
-Rails.application.config.session_store :cookie_store,
-  key: "_omoide_gift_session",
-  secure: Rails.env.production?,
-  same_site: (Rails.env.production? ? :none : :lax),
-  domain: (Rails.env.production? ? ".meguri-gift.com" : nil)
