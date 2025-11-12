4cf91afe9ae3a6685a6f379b88778a850161efec
shun <samesamejinbei@gmail.com>
2025-11-12 13:55:53 +0900

    fix:追加したが、不要なため削除

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-if Rails.env.production?
-  # Ensure OmniAuth generates callback URLs with the correct HTTPS host
-  OmniAuth.config.full_host = "https://meguri-gift.com"
-end
