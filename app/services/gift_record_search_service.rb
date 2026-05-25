45767dc24ed64365c171087f6fe4cbb68f272b91
shun <samesamejinbei@gmail.com>
2026-05-25 10:43:11 +0900

    update: 検索上限を50件に変更し人名フィルタリングを追加

--- app/services/gift_record_search_service.rb
+++ app/services/gift_record_search_service.rb
@@ -1,5 +1,5 @@
 class GiftRecordSearchService
-  DEFAULT_LIMIT = 5
+  DEFAULT_LIMIT = 50
 
   def self.search(query:, user:, limit: DEFAULT_LIMIT)
     query_vector = EmbeddingService.embed(query)
@@ -7,10 +7,23 @@ class GiftRecordSearchService
 
     vector_literal = "[#{query_vector.map { |v| Float(v) }.join(',')}]"
 
-    GiftRecord
+    scope = GiftRecord
       .where(user: user)
       .where.not(embedding: nil)
+
+    matched_people = find_people_in_query(query, user)
+    scope = scope.where(gift_people_id: matched_people.map(&:id)) if matched_people.any?
+
+    scope
       .order(Arel.sql("embedding <=> '#{vector_literal}'::vector"))
       .limit(limit)
+  end
+
+  class << self
+    private
+
+    def find_people_in_query(query, user)
+      GiftPerson.where(user: user).select { |p| query.include?(p.name) }
+    end
   end
 end
