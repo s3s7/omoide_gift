2a9c275cefd081881295e132cef223af6add9d22
shun <samesamejinbei@gmail.com>
2026-05-25 10:40:29 +0900

    style: 空白行を削除

--- app/jobs/generate_embedding_job.rb
+++ app/jobs/generate_embedding_job.rb
@@ -4,7 +4,6 @@ class GenerateEmbeddingJob < ApplicationJob
   def perform(gift_record_id)
     gift_record = GiftRecord.find_by(id: gift_record_id)
     return unless gift_record
-
     EmbeddingService.generate_and_save(gift_record)
   end
 end
